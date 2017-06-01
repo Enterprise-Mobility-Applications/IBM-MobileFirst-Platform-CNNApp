a8598137a4d43b82fd960a86ec3dc617c626a927
Reddy <niranjan.a.reddy@capgemini.com>
2017-06-01 19:01:54 +0530

    Server side script added

--- apps/cnnRSSHybrid/android/native/assets/www/default/worklight/checksum.js
+++ apps/cnnRSSHybrid/android/native/assets/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"date":1496317812590,"machine":"10.103.2.101","checksum":2272952941};
-/* Date: Thu Jun 01 17:20:12 IST 2017 */
+var WL_CHECKSUM = {"date":1496323867430,"machine":"10.103.2.101","checksum":3452693787};
+/* Date: Thu Jun 01 19:01:07 IST 2017 */
