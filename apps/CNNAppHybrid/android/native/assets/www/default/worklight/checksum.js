f487743540a5a5a46ac72c9982c1f10b23e8b696
Reddy <niranjan.a.reddy@capgemini.com>
2017-06-02 17:41:24 +0530

    UI Modified for Regions (NEWS)

--- apps/CNNAppHybrid/android/native/assets/www/default/worklight/checksum.js
+++ apps/CNNAppHybrid/android/native/assets/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"date":1496402508081,"machine":"10.103.1.91","checksum":199481523};
-/* Date: Fri Jun 02 16:51:48 IST 2017 */
+var WL_CHECKSUM = {"date":1496405404902,"machine":"10.103.1.91","checksum":973125825};
+/* Date: Fri Jun 02 17:40:04 IST 2017 */
--- apps/cnnRSSHybrid/android/native/assets/www/default/worklight/checksum.js
+++ apps/cnnRSSHybrid/android/native/assets/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"date":1496398877489,"machine":"10.103.1.91","checksum":2365951108};
-/* Date: Fri Jun 02 15:51:17 IST 2017 */
+var WL_CHECKSUM = {"date":1496405418063,"machine":"10.103.1.91","checksum":2365951108};
+/* Date: Fri Jun 02 17:40:18 IST 2017 */
