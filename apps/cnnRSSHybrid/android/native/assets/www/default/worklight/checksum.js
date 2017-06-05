5f9e28b8818a34c45e7da5e69c4a96aeb83f97af
Reddy <niranjan.a.reddy@capgemini.com>
2017-06-05 16:03:14 +0530

    Added native android icons and splash screen

--- apps/cnnRSSHybrid/android/native/assets/www/default/worklight/checksum.js
+++ apps/cnnRSSHybrid/android/native/assets/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"date":1496658254234,"machine":"10.102.19.90","checksum":2365951108};
-/* Date: Mon Jun 05 15:54:14 IST 2017 */
+var WL_CHECKSUM = {"date":1496658691819,"machine":"10.102.19.90","checksum":2365951108};
+/* Date: Mon Jun 05 16:01:31 IST 2017 */
