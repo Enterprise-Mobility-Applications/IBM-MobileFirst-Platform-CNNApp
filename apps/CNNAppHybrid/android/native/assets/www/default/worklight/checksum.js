bb10ed0d93c27bbc999ba78fdb69774d620fe8b9
Reddy <niranjan.a.reddy@capgemini.com>
2017-06-05 15:54:55 +0530

    Icon and Thumbnail for App

--- apps/CNNAppHybrid/android/native/assets/www/default/worklight/checksum.js
+++ apps/CNNAppHybrid/android/native/assets/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"date":1496657546822,"machine":"10.102.19.90","checksum":1259175161};
-/* Date: Mon Jun 05 15:42:26 IST 2017 */
+var WL_CHECKSUM = {"date":1496658241961,"machine":"10.102.19.90","checksum":1952530428};
+/* Date: Mon Jun 05 15:54:01 IST 2017 */
--- apps/cnnRSSHybrid/android/native/assets/www/default/worklight/checksum.js
+++ apps/cnnRSSHybrid/android/native/assets/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"date":1496657559717,"machine":"10.102.19.90","checksum":2365951108};
-/* Date: Mon Jun 05 15:42:39 IST 2017 */
+var WL_CHECKSUM = {"date":1496658254234,"machine":"10.102.19.90","checksum":2365951108};
+/* Date: Mon Jun 05 15:54:14 IST 2017 */
